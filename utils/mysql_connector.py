56c70d6d250a6f6a38bbebeb4187ddbeda63a8c4
marceines <chelita141@gmail.com>
2023-10-28 15:15:26 -0400

    Adding last changes

--- utils/mysql_connector.py
+++ utils/mysql_connector.py
@@ -3,16 +3,27 @@ import mysql.connector
 
 class MYSQLConnector:
 
-    def __int__(self):
+    def __init__(self):
         self.mydb = mysql.connector.connect(
             host="localhost",
-            user="",
-            password="",
-            database=""
+            user="root",
+            password="Planet1@",
+            database="todo_data"
         )
         print(self.mydb)
+
+    def execute_query(self, sql, param=None):
+        cursor = self.mydb.cursor()
+        cursor.execute(sql, param)
+        result = cursor.fetchall()
+
+        return result
 
 
 if __name__ == '__main__':
     db = MYSQLConnector()
-
+    name = ('projects_get_200', )
+    query = "SELECT * FROM todo_data.input_data; where name = %s"
+    res = db.execute_query(query, param=name)
+    for i in res:
+        print(i)
